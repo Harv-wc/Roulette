d0c21e63c893443aa7fc7858e8a981571ae60d55
William <harv.wc@gmail.com>
2019-08-25 15:07:55 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Roulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 15; i++)
+            {
+                RouletteBall ball = new RouletteBall { };
+                Console.WriteLine(ball.Number);
+            }
+        }
+    }
+
+    class RouletteBall
+    {
+        Random rand = new Random();
+        public int Number { get; }
+        public RouletteBall()
+        {
+            Number = rand.Next(37);
+        }
+    }
+}
