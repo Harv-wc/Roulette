d0c4a5164eb63145664675403b1ce3762bd0b15a
William <harv.wc@gmail.com>
2019-08-25 19:41:31 -0400

    Only missing split and corner bets

--- Menu.cs
+++ Menu.cs
@@ -6,8 +6,6 @@ namespace Roulette
 {
     class Menu
     {
-        
-        public ConsoleKeyInfo UserInput2 { get; set; }
         public void DisplayOptions()
         {
             Console.WriteLine($"Select a bet to make:\n\n" +
@@ -62,25 +60,159 @@ namespace Roulette
                 return red;
 
         }
-        public void OptionHighOrLow()
+        public int OptionHighOrLow()
         {
-
+            int selection;
+            Console.Clear();
+            Console.WriteLine($"1. For low bet: 1 - 18 including 0\n" +
+                $"or\n" +
+                $"2. For high bet: 19 - 36 including 00\n");
+            selection = Convert.ToInt32(Console.ReadLine());
+            return selection;
         }
-        public void OptionDozens()
+        public int OptionDozens()
         {
-
+            int selection;
+            Console.Clear();
+            Console.WriteLine($"1. For first dozen: 1 - 12\n" +
+                $"2. For second dozen: 13 - 24\n" +
+                $"3. For third dozen: 25 - 36");
+            selection = Convert.ToInt32(Console.ReadLine());
+            return selection;
         }
-        public void OptionColumns()
+        public int[] OptionColumns()
         {
-
+            int selection;
+            int[] first = { 1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34 };
+            int[] second = { 2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35 };
+            int[] third = { 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36 };
+            Console.Clear();
+            Console.WriteLine($"1. Column 1\n" +
+                $"2. Column 2\n" +
+                $"3. Column 3\n");
+            selection = Convert.ToInt32(Console.ReadLine());
+            if (selection == 1)
+                return first;
+            else if (selection == 2)
+                return second;
+            else
+                return third;
         }
-        public void OptionStreet()
+        public int[] OptionStreet()
         {
-
+            int selection;
+            int[] s1 = { 1, 2, 3 };
+            int[] s2 = { 4, 5, 6 };
+            int[] s3 = { 7, 8, 9 };
+            int[] s4 = { 10, 11, 12 };
+            int[] s5 = { 13, 14, 15 };
+            int[] s6 = { 16, 17, 18 };
+            int[] s7 = { 19, 20, 21 };
+            int[] s8 = { 22, 23, 24 };
+            int[] s9 = { 25, 26, 27 };
+            int[] s10 = { 28, 29, 30 };
+            int[] s11 = { 31, 32, 33 };
+            int[] s12 = { 34, 35, 36 };
+            Console.Clear();
+            Console.WriteLine($"Choose the street to bet on:\n" +
+                $"1. 1, 2, 3\n" +
+                $"2. 4, 5, 6\n" +
+                $"3. 7, 8, 9\n" +
+                $"4. 10, 11, 12\n" +
+                $"5. 13, 14, 15\n" +
+                $"6. 16, 17, 18\n" +
+                $"7. 19, 20, 21\n" +
+                $"8. 22, 23, 24\n" +
+                $"9. 25, 26, 27\n" +
+                $"10. 28, 29, 30\n" +
+                $"11. 31, 32, 33\n" +
+                $"12. 34, 35, 36\n");
+            selection = Convert.ToInt32(Console.ReadLine());
+            switch (selection)
+            {
+                case 1:
+                    return s1;
+                case 2:
+                    return s2;
+                case 3:
+                    return s3;
+                case 4:
+                    return s4;
+                case 5:
+                    return s5;
+                case 6:
+                    return s6;
+                case 7:
+                    return s7;
+                case 8:
+                    return s8;
+                case 9:
+                    return s9;
+                case 10:
+                    return s10;
+                case 11:
+                    return s11;
+                case 12:
+                    return s12;
+                default:
+                    return null;
+            }
         }
-        public void OptionDoubleStreet()
+        public int[] OptionDoubleStreet()
         {
-
+            int selection;
+            int[] s1 = { 1, 2, 3, 4, 5, 6 };
+            int[] s2 = { 4, 5, 6, 7, 8, 9 };
+            int[] s3 = { 7, 8, 9, 10, 11, 12 };
+            int[] s4 = { 10, 11, 12, 13, 14, 15 };
+            int[] s5 = { 13, 14, 15, 16, 17, 18 };
+            int[] s6 = { 16, 17, 18, 19, 20, 21 };
+            int[] s7 = { 19, 20, 21, 22, 23, 24 };
+            int[] s8 = { 22, 23, 24, 25, 26, 27 };
+            int[] s9 = { 25, 26, 27, 28, 29, 30 };
+            int[] s10 = { 28, 29, 30, 31, 32, 33 };
+            int[] s11 = { 31, 32, 33, 34, 35, 36 };
+            Console.Clear();
+            Console.WriteLine($"Choose the double-street to bet on:\n" +
+                $"1. 1, 2, 3, 4, 5, 6\n" +
+                $"2. 4, 5, 6, 7, 8, 9\n" +
+                $"3. 7, 8, 9, 10, 11, 12\n" +
+                $"4. 10, 11, 12, 13, 14, 15\n" +
+                $"5. 13, 14, 15, 16, 17, 18\n" +
+                $"6. 16, 17, 18, 19, 20, 21\n" +
+                $"7. 19, 20, 21, 22, 23, 24\n" +
+                $"8. 22, 23, 24, 25, 26, 27\n" +
+                $"9. 25, 26, 27, 28, 29, 30\n" +
+                $"10. 28, 29, 30, 31, 32, 33\n" +
+                $"11. 31, 32, 33, 34, 35, 36\n");
+            selection = Convert.ToInt32(Console.ReadLine());
+            switch (selection)
+            {
+                case 1:
+                    return s1;
+                case 2:
+                    return s2;
+                case 3:
+                    return s3;
+                case 4:
+                    return s4;
+                case 5:
+                    return s5;
+                case 6:
+                    return s6;
+                case 7:
+                    return s7;
+                case 8:
+                    return s8;
+                case 9:
+                    return s9;
+                case 10:
+                    return s10;
+                case 11:
+                    return s11;
+                default:
+                    return null;
+            }
         }
         public void OptionSplit()
         {
--- Program.cs
+++ Program.cs
@@ -15,6 +15,11 @@ namespace Roulette
             List<int> exactBets = new List<int>();
             int evenOddBet = 0;
             int[] colorBet = new int[18];
+            int highLow = -1;
+            int dozens = -1;
+            int[] column = new int[12];
+            int[] street = new int[3];
+            int[] doubleStreet = new int[6];
 
 
             do
@@ -42,14 +47,24 @@ namespace Roulette
                         Console.Clear();
                         break;
                     case 4:
+                        highLow = placeBets.OptionHighOrLow(); // 1 is low    2 is high
+                        Console.Clear();
                         break;
                     case 5:
+                        dozens = placeBets.OptionDozens(); // 1 = first, 2 = second, 3 = third  of dozen group
+                        Console.Clear();
                         break;
                     case 6:
+                        column = placeBets.OptionColumns();
+                        Console.Clear();
                         break;
                     case 7:
+                        street = placeBets.OptionStreet();
+                        Console.Clear();
                         break;
                     case 8:
+                        doubleStreet = placeBets.OptionDoubleStreet();
+                        Console.Clear();
                         break;
                     case 9:
                         break;
@@ -66,13 +81,14 @@ namespace Roulette
             RouletteBall ball = new RouletteBall { };
             Console.WriteLine($"Roulette ball number: {ball.Number}\n");
             // bets from option 1: exact numbers
-            Console.Write($"Your exact bets: \n");
             foreach (int num in exactBets)
             {
                 if (ball.Number == num)
                 {
                     Console.WriteLine($"{num} is a winning bet!");
                 }
+                if (ball.Number == 37 && num == 00)
+                    Console.WriteLine($"{num} is a winning bet!");
             }
             //
 
@@ -96,6 +112,59 @@ namespace Roulette
                 }
             }
             //
+            
+            //bet from option 4: high or low numbers
+            if (highLow == 1 && ball.Number >= 0 && ball.Number <= 18)
+            {
+                Console.WriteLine($"Your low bet wins!");
+            }
+            if (highLow == 2 && ball.Number >= 19 && ball.Number <= 37)
+            {
+                Console.WriteLine($"Your high bet wins!");
+            }
+            //
+
+            //bet from option 5: groups of dozens
+            if (dozens == 1 && ball.Number >= 1 && ball.Number <= 12)
+            {
+                Console.WriteLine($"Your first dozen bet (1-12) wins!");
+            }
+            if (dozens == 2 && ball.Number >= 13 && ball.Number <= 24)
+            {
+                Console.WriteLine($"Your second dozen bet (13-24) wins!");
+            }
+            if (dozens == 3 && ball.Number >= 25 && ball.Number <= 36)
+            {
+                Console.WriteLine($"Your third dozen bet (25-36) wins!");
+            }
+            //
+
+            //bet from option 6: column bet
+            foreach (int num in column)
+            {
+                if (ball.Number == num)
+                {
+                    Console.WriteLine("Your column bet wins!");
+                }
+            }
+            //
+
+            //bet from option 7: street bet
+            foreach (int num in street)
+            {
+                if (ball.Number == num)
+                    Console.WriteLine($"Your street bet wins!");
+            }
+            //
+
+            //bet from option 8: double-street bet
+            foreach (int num in doubleStreet)
+            {
+                if (ball.Number == num)
+                    Console.WriteLine($"Your double-street bet wins!");
+            }
+            //
+
 
         }
     }
