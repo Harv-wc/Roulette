13d27b72ca2d35aefa44cba495b04a2dd77d2f88
William <harv.wc@gmail.com>
2019-08-25 18:13:37 -0400

    Changed method for calculating bets

--- Menu.cs
+++ Menu.cs
@@ -6,6 +6,8 @@ namespace Roulette
 {
     class Menu
     {
+        
+        public ConsoleKeyInfo UserInput2 { get; set; }
         public void DisplayOptions()
         {
             Console.WriteLine($"Select a bet to make:\n\n" +
@@ -19,46 +21,72 @@ namespace Roulette
                 $"8. Double Street...\n" +
                 $"9. Split...\n" +
                 $"10. Corner...\n" +
+                $"0. To Stop All Bets!\n" +
                 $"####################");
         }
 
-        public void DisplayExactNumber()
+        public int OptionExactNumber()
+        {
+            int bet;
+            Console.Clear();
+            Console.Write($"Enter the number you want to place your bet on: ");
+            bet = Convert.ToInt32(Console.ReadLine());
+            return bet;
+            
+        }
+        public int OptionEvenOrOdd()
+        {
+            int evenOrOdd;
+            Console.Clear();
+            Console.WriteLine($"1. Even\n" +
+                $"or\n" +
+                $"2. Odd");
+            evenOrOdd = Convert.ToInt32(Console.ReadLine());
+            return evenOrOdd;
+        }
+        public int[] OptionBlackOrRed()
+        {
+            int selection;
+            int[] black = { 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35 };
+            int[] red = { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
+            Console.Clear();
+            Console.WriteLine($"1. Black\n" +
+                $"or\n" +
+                $"2. Red\n");
+            selection = Convert.ToInt32(Console.ReadLine());
+            if (selection == 1)
+            {
+                return black;
+            }
+            else
+                return red;
+
+        }
+        public void OptionHighOrLow()
         {
 
         }
-        public void DisplayEvenOrOdd()
+        public void OptionDozens()
         {
 
         }
-        public void DisplayBlackOrRed()
+        public void OptionColumns()
         {
 
         }
-        public void DisplayHighOrLow()
+        public void OptionStreet()
         {
 
         }
-        public void DisplayDozens()
+        public void OptionDoubleStreet()
         {
 
         }
-        public void DisplayColumns()
+        public void OptionSplit()
         {
 
         }
-        public void DisplayStreet()
-        {
-
-        }
-        public void DisplayDoubleStreet()
-        {
-
-        }
-        public void DisplaySplit()
-        {
-
-        }
-        public void DisplayCorner()
+        public void OptionCorner()
         {
 
         }
--- Program.cs
+++ Program.cs
@@ -1,19 +1,102 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Roulette
 {
     class Program
     {
+        
+        public static int UserInput { get; set; }
         static void Main(string[] args)
         {
             Menu placeBets = new Menu { };
-            placeBets.DisplayOptions();
+            bool keepBetting = true;
+            //betting option results
+            List<int> exactBets = new List<int>();
+            int evenOddBet = 0;
+            int[] colorBet = new int[18];
 
-            for (int i = 0; i < 3; i++)
+
+            do
             {
-                RouletteBall ball = new RouletteBall { };
-                Console.WriteLine(ball.Number);
+
+                placeBets.DisplayOptions();
+                UserInput = Convert.ToInt16(Console.ReadLine());
+
+                switch (UserInput)
+                {
+                    case 0: // stop betting
+                        Console.Clear();
+                        keepBetting = false;
+                        break;
+                    case 1:
+                        exactBets.Add(placeBets.OptionExactNumber());
+                        Console.Clear();
+                        break;
+                    case 2:
+                        evenOddBet = placeBets.OptionEvenOrOdd(); // 1 = even  |  2 = odd
+                        Console.Clear();
+                        break;
+                    case 3:
+                        colorBet = placeBets.OptionBlackOrRed();
+                        Console.Clear();
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Wrong selection. Lets try that again...");
+                        Console.ReadLine();
+                        break;
+                }
+            } while (keepBetting);
+
+            RouletteBall ball = new RouletteBall { };
+            Console.WriteLine($"Roulette ball number: {ball.Number}\n");
+            // bets from option 1: exact numbers
+            Console.Write($"Your exact bets: \n");
+            foreach (int num in exactBets)
+            {
+                if (ball.Number == num)
+                {
+                    Console.WriteLine($"{num} is a winning bet!");
+                }
             }
+            //
+
+            //bet from option 2: even or odd
+            if (evenOddBet == 1 && ball.Number%2 == 0)
+            {
+                Console.WriteLine($"Your Even bet wins!");
+            }
+            else if (evenOddBet == 2 && ball.Number%2 != 0)
+            {
+                Console.WriteLine($"Your Odd bet wins!");
+            }
+            //
+
+            //bet from option 3: color black or red
+            foreach (int num in colorBet)
+            {
+                if (ball.Number == num)
+                {
+                    Console.WriteLine("Your color bet wins!");
+                }
+            }
+            //
+
         }
     }
 
@@ -26,9 +109,6 @@ namespace Roulette
             Number = rand.Next(37);
         }
     }
-    class MadeBets
-    {
-        // stores numbers from bets made to compare against BallNumber
-    }
+   
 
 }
